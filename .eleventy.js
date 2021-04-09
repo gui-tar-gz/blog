9c8d9e99ade07b876d9cbb5f1377ada55d72068e
gui_tar_gz <alexander.gruzdev@gmail.com>
2021-04-09 20:13:34 +0200

    change .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -57,8 +57,8 @@ module.exports = function(eleventyConfig) {
   });
 
   // Copy the `img` and `css` folders to the output
-  eleventyConfig.addPassthroughCopy("img");
-  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("src/img");
+  eleventyConfig.addPassthroughCopy("src/css");
 
   // Customize Markdown library and settings:
   let markdownLibrary = markdownIt({
@@ -76,7 +76,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.setBrowserSyncConfig({
     callbacks: {
       ready: function(err, browserSync) {
-        const content_404 = fs.readFileSync('_site/404.html');
+        const content_404 = fs.readFileSync('src/404.html');
 
         browserSync.addMiddleware("*", (req, res) => {
           // Provides the 404 content without redirect.
