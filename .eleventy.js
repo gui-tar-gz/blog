1a183049291f64273840fffa756720672370ffab
gui_tar_gz <alexander.gruzdev@gmail.com>
2021-04-09 21:02:02 +0200

    change .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -111,7 +111,7 @@ module.exports = function(eleventyConfig) {
     // You can also pass this in on the command line using `--pathprefix`
 
     // Optional (default is shown)
-    pathPrefix: "dist",
+    pathPrefix: "/",
     // -----------------------------------------------------------------
 
     // Pre-process *.md files with: (default: `liquid`)
