63ab4174c12e2410b87b092a09f55a558f1913eb
gui_tar_gz <alexander.gruzdev@gmail.com>
2021-04-10 18:33:47 +0200

    apply new theme

--- .eleventy.js
+++ .eleventy.js
@@ -22,6 +22,10 @@ module.exports = function(eleventyConfig) {
     return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
   });
 
+  eleventyConfig.addFilter("year", dateObj => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("yyyy");
+  });
+
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
   eleventyConfig.addFilter('htmlDateString', (dateObj) => {
     return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
--- src/about/index.md
+++ src/about/index.md
@@ -7,4 +7,4 @@ eleventyNavigation:
   order: 3
 ---
 
-Hi! I'm Alex. This blog is based on <a href="{{ 'https://www.11ty.dev/' | url }}">Eleventy.</a>.
+Hi! I'm Alex. This blog is based on <a href="{{ 'https://www.11ty.dev/' | url }}">Eleventy</a>.
